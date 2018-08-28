7c80bc0a146e1e88f010dbb7d6d4c6d357f0cceb
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-08-28 12:08:15 -0400

    Update demo1.tf

--- demo1.tf
+++ demo1.tf
@@ -7,4 +7,20 @@ provider "aws" {
  resource "aws_security_group" "jenkins-pipeline-sg" {
   name        = "jenkins-pipeline-sg"
   description = "built-by-jenkins-pipeline"
+   
+   vpc_id      = "vpc-6537bf0d"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
