0d998d0a2dee328e9b8f07e1a4c676fd2bbca1f4
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-08-28 11:19:33 -0400

    Delete firsttry.tf

--- firsttry.tf
+++
@@ -1,9 +0,0 @@
-
-provider "aws" {
-  region = "ca-central-1"
-}
-
-resource "aws_security_group" "jenkins-pipeline-sg" {
-  name        = "jenkins-pipeline-sg"
-  description = "built-by-jenkins-pipeline"
-}
