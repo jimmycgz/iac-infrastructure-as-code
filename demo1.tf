2b32300ad29018dbfcd784da57ad8940b4c1ebd7
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-08-28 11:19:16 -0400

    Create demo1.tf

---
+++ demo1.tf
@@ -0,0 +1,33 @@
+pipeline {
+    /*  Get Terraform resource file by checking out SCM*/
+    /* Create VPC by Terraform resource file */
+    
+    agent any
+
+environment {
+        TERRAFORM_CMD = 'terraform'
+    }
+    stages {
+        stage('checkout repo') {
+            steps {
+              checkout scm
+            }
+        }
+     
+        stage('plan') {
+            steps {
+                sh  """
+                    ${TERRAFORM_CMD} plan -out=tfplan -input=false 
+                    """
+                
+                }
+           }
+        stage('apply') {
+            steps {
+                sh  """
+                    ${TERRAFORM_CMD} apply -lock=false -input=false tfplan
+                    """
+                  }
+                }
+    }
+}
