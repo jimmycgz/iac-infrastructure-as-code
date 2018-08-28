6866f19111598295b3d0f1dd76e76018cb2b6cce
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-08-28 11:37:27 -0400

    Update demo1.tf

--- demo1.tf
+++ demo1.tf
@@ -1,4 +1,7 @@
 provider "aws" {
+  
+  access_key = "${var.aws_access_key_id}"
+  secret_key = "${var.aws_secret_access_key}"
   region = "ca-central-1"
 }
  resource "aws_security_group" "jenkins-pipeline-sg" {
