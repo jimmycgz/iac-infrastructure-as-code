a278cb9fba9e1322659b8d8f165a3ca50cb0edc6
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-08-29 20:05:58 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "terra-jmy-bucket"
     key    = "mystate"
     region = "ca-central-1"
+    shared_credentials_file = "/home/ubuntu/.aws/credentials"
+  profile                 = "default"
   }
 }
