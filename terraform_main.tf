7e1df9a848c42581b0fcec96c45b3b4b0c25e3b0
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-09-03 11:07:51 -0400

    Update terraform_main.tf

--- terraform_main.tf
+++ terraform_main.tf
@@ -134,34 +134,11 @@ resource "aws_eip_association" "j_t_eip2_asso" {
 }
 
 
-# Configure the Chef provider
-provider "chef" {
-  server_url = "https://api.chef.io/organizations/example/"
+#
 
-  # You can set up a "Client" within the Chef Server management console.
-  client_name  = "terraform"
   key_material = "${file("../Jmy_Key_AWS_Apr_2018.pem")}"
 }
 
-# Create a Chef Environment
-#resource "chef_environment" "production" {
-#  name = "production"
-#}
-
-# Create a Chef Role
-#resource "chef_role" "app_server" {
-#  name = "app_server"
-
-#  run_list = [
-#    "recipe[terra-chef.rb]",
-#  ]
-#}
-
-resource "chef_node" "j_t_API2" {
-  name             = "j_t_API2-environment"
-#  environment_name = "${chef_environment.j_t_API2.name}"
-  run_list         = ["recipe[terra-chef.rb]"]
-}
 
 
 
