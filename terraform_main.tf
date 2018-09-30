0bf280669ecbce598703c871ab63cc4fb16e052e
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-09-30 18:44:56 -0400

    Update terraform_main.tf

--- terraform_main.tf
+++ terraform_main.tf
@@ -133,7 +133,7 @@ resource "aws_elb" "jt-elb" {
 
   #subnets         = ["${aws_subnet.jt_pub_subnet.*.id}"]
   security_groups = ["${aws_security_group.jt_sg_elb.id}"]
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  availability_zones = ["${data.aws_availability_zones.available.names}"]
   instances       = ["${aws_instance.jt_api-aws.*.id}"]
 
   listener {
