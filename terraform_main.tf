08ca75c2a7169d9ad0869f7810e55d3cb4325b7d
Jimmy CUI <38402619+jimmycgz@users.noreply.github.com>
2018-10-01 17:51:47 -0400

    Update terraform_main.tf

--- terraform_main.tf
+++ terraform_main.tf
@@ -140,10 +140,11 @@ resource "aws_security_group" "jt-sg_demo1" {
 resource "aws_elb" "jt-elb" {
   name = "jt-demo-elb"
 
-  subnets         = ["${aws_subnet.jt-pub_subnet.*.id}"]
+  #Associate the elb to the instances in the first 2 subnets
+  subnets         = ["${list(aws_subnet.jt-pub_subnet.0.id, aws_subnet.jt-pub_subnet.1.id}"]
   security_groups = ["${aws_security_group.jt-sg_elb.id}"]
   #availability_zones = ["${data.aws_availability_zones.available.names}"]
-  instances       = ["${aws_instance.jt-api-aws.*.id}"]
+  instances       = ["${list(aws_instance.jt-api-aws.0.id, aws_instance.jt-api-aws.1.id}"]
 
   listener {
     instance_port     = 80
